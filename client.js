6c55ca7b79bf634176abd5bd24d4f708ba0d053a
tema <tema@vf8.ru>
2015-06-09 19:20:15 +0300

    Thrift client now works over HTTP

--- client.js
+++ client.js
@@ -2,19 +2,37 @@ var thrift = require('thrift');
 var Calculator = require('./gen-nodejs/Calculator');
 var ttypes = require('./gen-nodejs/calculator_types');
 
-transport = thrift.TBufferedTransport();
-protocol = thrift.TJSONProtocol();
+//transport = thrift.TBufferedTransport();
+//protocol = thrift.TJSONProtocol();
 
-var connection = thrift.createConnection('127.0.0.1', 1337, {
-  transport: transport,
-  protocol: protocol
-});
+
+var options = {
+  transport: thrift.TBufferedTransport,
+  protocol: thrift.TJSONProtocol,
+  path: '/',
+  headers: {"Connection": "close"}
+};
+
+var connection = thrift.createHttpConnection("localhost", 13370, options);
+var client = thrift.createHttpClient(Calculator, connection);
+
+//
+//
+//var connection = thrift.createHttpConnection('127.0.0.1', 1337, {
+//  transport: transport,
+//  protocol: protocol
+//});
 
 connection.on('error', function (err) {
   assert(false, err);
 });
 
-var client = thrift.createClient(Calculator, connection);
+
+
+
+
+
+
 
 client.ping(function (err, response) {
   console.log('ping()');
@@ -25,3 +43,8 @@ client.add(1, 1, function (err, response) {
   console.log("1+1=" + response);
   //connection.end();
 });
+
+
+setTimeout(function() {
+  console.log('finish');
+}, 5000);
